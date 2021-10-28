4ab75fcc96c83cfc93bb1a90d4d942fedda4e2ad
Samuel VIDEAU <samuelvideau@yahoo.fr>
2021-10-28 16:37:47 +0200

    feat: token unit mint

--- contracts/NFcool.sol
+++ contracts/NFcool.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol
 contract NFcool is ERC1155PresetMinterPauser {
     using Strings for uint256;
 
-    uint8 tokensCount;
-
     struct TokenData {
         string uri;
         string name;
@@ -16,7 +14,11 @@ contract NFcool is ERC1155PresetMinterPauser {
     struct TokenUnitData {
         address owner;
         string nfcId;
+        string status;
     }
+
+    uint8 tokensCount;
+    mapping (uint256 => uint256) private _tokenUnitsCount;
 
     mapping(uint256 => TokenData) private _tokenData;
 
@@ -37,14 +39,23 @@ contract NFcool is ERC1155PresetMinterPauser {
         return keccak256(bytes(_tokenData[tokenId].name)) != keccak256(bytes(""));
     }
 
-    function createToken(string calldata tokenUri, string calldata tokenName) external returns (uint256) {
+    function mint(string calldata tokenUri, string calldata tokenName, bytes memory data) external returns (uint256) {
         require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
 
-        _mint(_msgSender(), tokensCount, 0, "");
+        _mint(address(this), tokensCount, 0, data);
         _tokenData[tokensCount] = TokenData(tokenUri, tokenName);
 
         tokensCount++;
         return tokensCount;
     }
 
+    function mintUnit(uint256 tokenId, string calldata nfcId, bytes memory data) external returns (uint256) {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+
+        _mint(address(this), _tokenUnitsCount[tokenId], 1, data);
+        _tokenUnitData[tokenId][_tokenUnitsCount[tokenId]] = TokenUnitData(address(this), nfcId, "minted");
+
+        _tokenUnitsCount[tokenId]++;
+        return _tokenUnitsCount[tokenId];
+    }
 }
