62b5a907f88c12e58417ffa789b6125e6030774e
Samuel VIDEAU <samuelvideau@yahoo.fr>
2021-10-29 12:39:57 +0200

    test: new coverages
    Took 58 minutes

--- test/NFCool.test.js
+++ test/NFCool.test.js
@@ -1,3 +1,4 @@
+const {shouldThrow} = require("./helpers/utils");
 const NFCool = artifacts.require("NFCool");
 const expect = require('chai').expect;
 
@@ -8,27 +9,88 @@ contract("NFCool", (accounts) => {
     beforeEach(async () => {
         contract = await NFCool.new({from: owner});
     });
-    context("mint a token", async () => {
-       let res;
-       let tokenName = "AirMax 2000";
-       let tokenUri = "testUri";
+    context("mint tokens", async () => {
+       let ownerMints = [];
+       const tokens = [
+           {
+               name: "AirMax 2020",
+               uri: "testURI"
+           },
+           {
+               name: "AirMax 2021",
+               uri: "testURI2"
+           }
+       ];
 
-       beforeEach(async() => {
-          res = await contract.mintToken(tokenUri, tokenName, web3.utils.asciiToHex(""), {from: owner});
-       });
+        beforeEach(async() => {
+            for (let i = 0 ; i < tokens.length ; i++) {
+                ownerMints.push(await contract.mintToken(tokens[i].uri, tokens[i].name, web3.utils.asciiToHex(""), {from: owner}));
+            }
+        });
 
-       it ("Should not throw an error", () => {
-           expect(res.receipt.status).to.equal(true);
-       });
+        it ("Should not throw an error if owner", () => {
+            ownerMints.forEach(res => {
+                expect(res.receipt.status).to.equal(true);
+            });
+        });
 
-       it ("Should have expected uri", async () => {
-           const uri = (await contract.tokenData(0)).uri;
-            expect(uri).to.equal(tokenUri);
-       });
+        it ("Should throw an error if not owner", async () => {
+            await shouldThrow(contract.mintToken(tokens[0].uri, tokens[0].name, web3.utils.asciiToHex(""), {from: alice}));
+        });
+
+        it ("Should have expected uri", async () => {
+            for (let i = 0 ; i < tokens.length ; i++) {
+                const uri = (await contract.tokenData(i)).uri;
+                expect(uri).to.equal(tokens[i].uri);
+            }
+        });
 
         it ("Should have expected name", async () => {
-            const name = (await contract.tokenData(0)).name;
-            expect(name).to.equal(tokenName);
+            for (let i = 0 ; i < tokens.length ; i++) {
+                const name = (await contract.tokenData(i)).name;
+                expect(name).to.equal(tokens[i].name);
+            }
+        });
+
+        context ("Minting unit", async() => {
+           let unitsMinted = [];
+           let unitNfcIds = [];
+
+            beforeEach(async () => {
+                for (let i = 0 ; i < tokens ; i++) {
+                    unitsMinted.push([]);
+                    unitNfcIds.push([]);
+
+                    for (let j = 0 ; j < 2 ; j++) {
+                        let NFCId = (Math.random() % 9999).toString();
+                        unitNfcIds[i].push(NFCId);
+                        unitsMinted[i].push(await contract.mintTokenUnit(i, NFCId, web3.utils.asciiToHex(""), {from: owner}));
+                    }
+                }
+            });
+
+            it ("Should not Throw any error if owner", () => {
+                for (let i = 0 ; i < tokens ; i++) {
+                    for (let j = 0 ; j < 2 ; j++) {
+                       expect(unitsMinted[i][j].receipt.status).to.equal(true);
+                    }
+                }
+            });
+
+            it ("Should throw an error if not owner", async () => {
+                await shouldThrow(contract.mintTokenUnit(0, 'NFCId', web3.utils.asciiToHex(""), {from: alice}));
+            })
+
+            it ("Should have correct information stored", async () => {
+                for (let i = 0 ; i < tokens ; i++) {
+                    for (let j = 0 ; j < 2 ; j++) {
+                        const res = await contract.tokenUnitData(i, j);
+                        expect(res.status).to.equal('minted');
+                        expect(res.uri).to.equal(unitNfcIds[i][j]);
+                        expect(res.owner).to.equal(owner);
+                    }
+                }
+            });
         });
     });
 });
